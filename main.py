176248bc0a054bcf48c3580f04e48a71cf13bb52
thaytuh <htaylorkrass@gmail.com>
2025-03-09 17:23:34 -0700

    updated main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,6 +15,9 @@ def main():
                 return
         pygame.Surface.fill(screen, color="black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
 
     
 if __name__ == "__main__":
